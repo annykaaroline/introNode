991d2e4f7e938f13d88535170c15c13494607999
annykaaroline <134931526+annykaaroline@users.noreply.github.com>
2024-08-09 11:45:14 -0300

    09/08

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app = express();
 const porta = 3333;
 
 app.listen(porta, () => {
-    console.log('Servidor iniciado na porta' + porta);
-    //console.log(`Servidor iniciado na porta ${porta}`);
+    console.log('Servidor iniciado na porta ' + porta);
+    app.get('/', (request, response) => {
+        response.send('Hello World');
+    })
 });
